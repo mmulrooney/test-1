a962f8452afa7b80a4585d81b1e561ab515b0be4
Megan Mulrooney <meganrmulrooney@gmail.com>
2023-02-24 20:55:49 -0600

    first commit

--- test-1/ViewController.swift
+++ test-1/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+    let arrayOfQuotes = ["You are awesome!",
+                         "Be Yourself!",
+                         "You are amazing!",
+                         "Be kind, and go outside",
+                         "Nothing can stop you!","You are glowing!"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        let random_number = Int.random(in: 0...(arrayOfQuotes.count-1))
+        label.text=arrayOfQuotes[random_number]
+    }
+    
 }
 
